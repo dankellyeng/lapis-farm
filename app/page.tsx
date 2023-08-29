e18ddfaa79fed2bb3a3deec537b564ad80dcc074
dankellyeng <danielkelly27@gmail.com>
2023-08-29 16:44:02 +1000

    code clean and fixed height issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,8 @@ import "@radix-ui/themes/styles.css";
 import fetchEntries from "./utils/api/farmInfo";
 import page from "./page.module.scss";
 
-// async function fetchEntries() {
-//   const res = await fetch(
-//     `https://s4lajcdqnnp3yx3xwnk4nstbtm0zcekl.lambda-url.us-east-1.on.aws`
-//   );
-
-//   if (!res.ok) {
-//     throw new Error("failed to fetch");
-//   }
-//   // console.log(res.json());
-
-//   return res.text();
-// }
-
 export default async function Home() {
   const data = await fetchEntries();
-  // let projects = data.items;
 
   return (
     <div className={page.fullPage}>
